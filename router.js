00acd8fd0c702cb78e0e194eedcdb432407d750e
Pranas Varaneckas <varaneckas@gmail.com>
2021-10-17 17:31:11 +0300

    fixed not working url validation

--- router.js
+++ router.js
@@ -16,37 +16,40 @@ router
     .post("/shorturl", (req, res) => {
         const url = req.body.url;
         if (isValidUrl(url)) {
-            UrlModel.findOne({url}, (err, foundUrl) => {
-                if (err) console.error(err);
-                if (foundUrl) {
-                    res.json({
-                        original_url: foundUrl.url,
-                        short_url: foundUrl.shortenedUrl
-                    })
-                } else {
-                    UrlModel
-                        .findOne()
-                        .sort("-shortenedUrl")
-                        .exec((err, lastUrl) => {
-                            if (err) console.error(err);
-                            const newUrl = new UrlModel({
-                                url,
-                                shortenedUrl: ++lastUrl.shortenedUrl
-                            });
-                            newUrl.save((err, savedUrl) => {
+            dns.lookup(trimUrl(url), (err, address, family) => {
+                if (err) {
+                    return returnError(res);
+                }
+                UrlModel.findOne({url}, (err, foundUrl) => {
+                    if (err) console.error(err);
+                    if (foundUrl) {
+                        res.json({
+                            original_url: foundUrl.url,
+                            short_url: foundUrl.shortenedUrl
+                        })
+                    } else {
+                        UrlModel
+                            .findOne()
+                            .sort("-shortenedUrl")
+                            .exec((err, lastUrl) => {
                                 if (err) console.error(err);
-                                res.json({
-                                    original_url: savedUrl.url,
-                                    short_url: savedUrl.shortenedUrl
+                                const newUrl = new UrlModel({
+                                    url,
+                                    shortenedUrl: ++lastUrl.shortenedUrl
+                                });
+                                newUrl.save((err, savedUrl) => {
+                                    if (err) console.error(err);
+                                    res.json({
+                                        original_url: savedUrl.url,
+                                        short_url: savedUrl.shortenedUrl
+                                    });
                                 });
                             });
-                        });
-                }
-            });
+                    }
+                });
+            })
         } else {
-            res.json({
-                error: "invalid url"
-            });
+           returnError(res);
         }
     })
     .get("/shorturl/:shortLink", (req, res) => {
@@ -61,13 +64,26 @@ router
         });
     });
 
+const returnError = res => {
+    res.json({
+       error: "invalid url"
+    });
+}
+
+const trimUrl = url => {
+    return url
+        .replace("http://", "")
+        .replace("https://", "")
+        .split("/")[0];
+}
+
 const isValidUrl = url => {
     const containsProtocol = url.includes("http://") || url.includes("https://");
     const containsWww = url.includes("www.");
     const containsDots = (url.match(/\./g) || []).length > 1;
     const containsNoTrailingDots = !url.includes("..");
 
-    return containsProtocol && containsWww && containsDots ;
+    return containsProtocol && containsWww && containsDots && containsNoTrailingDots;
 }
 
 module.exports = router;
